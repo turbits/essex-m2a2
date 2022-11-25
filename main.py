935e38a7843ef7cef716daea929959ab86f33c73
turbits <turbits@protonmail.com>
2022-11-25 12:48:08 -0700

    add base main function

---
+++ main.py
@@ -0,0 +1,38 @@
+# +===================================================================+
+# main.py - main function
+# +===================================================================+
+# Author: Trevor Woodman
+# Github: https://github.com/turbits
+# Repo: https://github.com/turbits/essex-m2a2
+# Project: Module 2, Assignment 2: System Implementation
+# Course: Object Oriented Programming (OOP_PCOM7E September 2022)
+# School: University of Essex
+# Date: September-December, 2022
+# +===================================================================+
+
+import signal
+import sys
+import time
+from vehicle import Vehicle
+from frontend import Frontend
+from backend import Backend
+
+def main():
+  vh = Vehicle()
+
+  program_running = True
+  frontend = Frontend(vh)
+  backend = Backend(vh)
+  update_tick = 1 # update every X seconds
+
+  while program_running:
+    frontend.update()
+    backend.update()
+
+    sys.stdout.write(frontend.get_vehicle_stats())
+    sys.stdout.flush()
+    
+    # update interval
+    time.sleep(update_tick)
+
+main()
