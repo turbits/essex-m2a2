3ffc514adfd73dd28e9fd2e2206ab4e8d5641359
turbits <turbits@protonmail.com>
2022-11-30 10:03:19 -0700

    wrapped main in Program class and added update loop with threading to run backend and frontend simultaneously

--- main.py
+++ main.py
@@ -14,24 +14,43 @@ import signal
 from sys import stdout
 import sys
 import time
+import threading
+from title import title_art
 from vehicle import Vehicle
 from frontend import Frontend
 from backend import Backend
+from utility import get_choice
 
-def main():
+class Program():
+  # threads will use this event to close
+  stop_event = threading.Event()
+  # instantiate our main vehicle
   vh = Vehicle()
-  stats = {}
-
-  program_running = True
-  frontend = Frontend(vh)
+  # update_tick dictates how quickly the simulation updates (front and backend)
+  update_tick = 1
   backend = Backend(vh)
-  update_tick = 1 # update every X seconds
+  frontend = Frontend(stop_event)
 
-  while program_running:
-    frontend.update()
-    backend.update()
-    
-    # update interval
-    time.sleep(update_tick)
+  # threading
+  frontend_thread = threading.Thread(target=frontend.main_menu)
+  frontend_thread.daemon = True
+  
+  # backend_thread = threading.Thread(target=backend.update)
+  # backend_thread.daemon = True
+  
 
-main()
+  def main(self):
+    self.frontend_thread.start()
+    # self.backend_thread.start()
+    while not self.stop_event.is_set():
+      # print "program tick"
+      # self.frontend.update()
+      self.backend.update()
+      # update interval
+      time.sleep(self.update_tick)
+
+    print "stop program"
+    sys.exit(0)
+
+prog = Program()
+prog.main()
