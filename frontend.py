7ca3fa0a8ded41dbe1ba2b29550d3aa7eb8884aa
turbits <turbits@protonmail.com>
2022-12-02 14:56:14 -0700

    add start/stop logic, cleaned up

--- frontend.py
+++ frontend.py
@@ -12,24 +12,39 @@
 
 from title import title_art
 from utility import get_choice
+import sys
 
 class Frontend():
-  stop_event = None
+  program = None
 
-  def __init__(self, e):
-    self.stop_event = e
+  def __init__(self, program):
+    self.program = program
+
 
   def main_menu(self):
     main_choices = ('Start Simulation', 'Stop Simulation', 'Vehicle Functions', 'Backend Data', 'Exit')
     print title_art
+    
+    print "---\nData Generation Running: {}\n---".format("Yes" if self.program.data_gen_on else "No")
+
     ch = get_choice(main_choices)
     if ch in ['exit', 4]:
-      self.stop_event.set()
+      self.program.stop_event.set()
+      print "Exiting program..."
+      sys.exit(0)
     elif ch in ['start simulation', 0]:
-      pass
+      self.program.start_data_generation()
+      return self.main_menu()
     elif ch in ['stop simulation', 1]:
-      pass
+      self.program.stop_data_generation()
+      return self.main_menu()
     elif ch in ['vehicle functions', 2]:
       pass
     elif ch in ['backend data', 3]:
       pass
+
+    return self.main_menu()
+
+  def start_frontend(self):
+    print "DEBUG: start_frontend"
+    return self.main_menu()
