b94407408b04e1053e11a48fbd0606b2da5e9688
turbits <turbits@protonmail.com>
2022-12-04 01:38:38 -0700

    fix issue with inputting non-alphanum chars in get_choice

--- utility.py
+++ utility.py
@@ -4,7 +4,7 @@
 # Author: Trevor Woodman
 # Github: https://github.com/turbits
 # Repo: https://github.com/turbits/essex-m2a2
-# Project: Module 2, Assignment 2: System Implementation
+# Project: Module 2 (OOP_PCOM7E), Assignment 2: System Implementation
 # Course: Object Oriented Programming (OOP_PCOM7E September 2022)
 # School: University of Essex
 # Date: September-December, 2022
@@ -26,11 +26,14 @@ def get_choice(list):
 
   user_input = raw_input("{} ".format(prompt))
   for count, value in enumerate(list):
-    if user_input == value:
+    if not user_input.isalnum():
+      print "invalid input; not alphanumeric"
+      return ""
+    elif user_input == value:
       return value.lower().strip()
     elif int(user_input) == count:
       return int(count)
-  print c_err("AVS-FRO-USER", "Invalid choice, please try again")
+  # print c_err("AVS-FRO-USER", "Invalid choice, please try again")
   return get_choice(list)
 
 def get_yn(question):
