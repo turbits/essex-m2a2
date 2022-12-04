fbe8fcc4b2f7722a72d1a2e004f2a7802245ddf8
turbits <turbits@protonmail.com>
2022-12-04 01:37:24 -0700

    major work on subsystems-overall logic in place

--- acc.py
+++ acc.py
@@ -4,7 +4,7 @@
 # Author: Trevor Woodman
 # Github: https://github.com/turbits
 # Repo: https://github.com/turbits/essex-m2a2
-# Project: Module 2, Assignment 2: System Implementation
+# Project: Module 2 (OOP_PCOM7E), Assignment 2: System Implementation
 # Course: Object Oriented Programming (OOP_PCOM7E September 2022)
 # School: University of Essex
 # Date: September-December, 2022
@@ -13,14 +13,49 @@
 from collections import deque
 
 class AdaptiveCruiseControl():
-  vehicle_in_range = False #bool
-  target_speed = 0
-  commands_queue = deque() #string queue
+  active = False
+  vehicle_in_range = False
+  target_speed = 100
+  # the command queue here is irrelevant really, but it was a requirement of the assignment
+  # to implement a queue. the commands are queued and processed in one function call
+  commands_queue = deque()
+  vh = None
 
-  #void
-  def acc_accelerate(self):
-    pass
+  def __init__(self, vehicle):
+    self.vh = vehicle
+  
+  def queue_command(self, command):
+    self.commands_queue.enqueue(command)
+  
+  def run_command(self):
+    self.commands_queue.dequeue()(self)
 
-  #void
-  def acc_decelerate(self):
-    pass
+  def maintain(self):
+    self.queue_command(self.vh.maintain(self.target_speed))
+    self.run_command()
+
+  def accelerate(self):
+    self.queue_command(self.vh.accelerate(self.target_speed))
+    self.run_command()
+
+  def decelerate(self):
+    self.queue_command(self.vh.brake(self.vh.speed - self.target_speed))
+    self.run_command()
+  
+  def activate(self):
+    self.active = True
+
+  def deactivate(self):
+    self.active = False
+
+  def update(self):
+    if self.active:
+      if self.vehicle_in_range:
+        self.maintain()
+      else:
+        # if target speed is greater than current speed, accelerate
+        if self.target_speed > self.vh.speed:
+          self.accelerate()
+        else:
+          # if current speed is greater than target speed, decelerate
+          self.decelerate()
--- aeb.py
+++ aeb.py
@@ -4,20 +4,38 @@
 # Author: Trevor Woodman
 # Github: https://github.com/turbits
 # Repo: https://github.com/turbits/essex-m2a2
-# Project: Module 2, Assignment 2: System Implementation
+# Project: Module 2 (OOP_PCOM7E), Assignment 2: System Implementation
 # Course: Object Oriented Programming (OOP_PCOM7E September 2022)
 # School: University of Essex
 # Date: September-December, 2022
 # +===================================================================+
 
 class AutomaticEmergencyBraking():
-  collision_detected = False #bool
-  entities_detected = [] #string list
+  collision_detected = False
+  distance_to_entity = 0
+  minimum_collision_distance = 5
+  vh = None
   
-  # bool
-  def detect_collision(self):
-    pass
+  def __init__(self, vehicle):
+    self.vh = vehicle
 
-  # void
   def emergency_brake(self):
-    pass
+    self.vh.brake(self.vh.speed)
+    self.vh.set_state(self.vh.state.IDLE)
+
+  def detect_collision(self):
+    # for row in self.detection_matrix:
+    #   for cell in row:
+    #     if cell == 1 and self.distance_to_entity <= self.minimum_collision_distance:
+    #       return True
+    # return False
+    if distance_to_entity <= self.minimum_collision_distance:
+      collision_detected = True
+      self.vh.entity_detected(distance=distance_to_entity)
+    else:
+      collision_detected = False
+      self.vh.entity_detected(clear=True)
+
+  def update(self):
+    if self.detect_collision():
+      self.emergency_brake()
