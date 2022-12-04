194164a6a6e17c6394a9eb66710a8e992895af31
turbits <turbits@protonmail.com>
2022-12-04 01:40:18 -0700

    add rest of main menu logic, cleaned up

--- frontend.py
+++ frontend.py
@@ -4,14 +4,14 @@
 # Author: Trevor Woodman
 # Github: https://github.com/turbits
 # Repo: https://github.com/turbits/essex-m2a2
-# Project: Module 2, Assignment 2: System Implementation
+# Project: Module 2 (OOP_PCOM7E), Assignment 2: System Implementation
 # Course: Object Oriented Programming (OOP_PCOM7E September 2022)
 # School: University of Essex
 # Date: September-December, 2022
 # +===================================================================+
 
 from title import title_art
-from utility import get_choice
+from utility import get_choice, c_err
 import sys
 
 class Frontend():
@@ -20,29 +20,32 @@ class Frontend():
   def __init__(self, program):
     self.program = program
 
-
   def main_menu(self):
-    main_choices = ('Start Simulation', 'Stop Simulation', 'Vehicle Functions', 'Backend Data', 'Exit')
+    main_choices = ('Start Simulation', 'Stop Simulation', 'Show Vehicle Functions', 'Show Backend Data Stream', 'Exit')
     print title_art
     
     print "---\nData Generation Running: {}\n---".format("Yes" if self.program.data_gen_on else "No")
 
     ch = get_choice(main_choices)
+    print ch
     if ch in ['exit', 4]:
       self.program.stop_event.set()
       print "Exiting program..."
-      sys.exit(0)
+      self.program.stop()
     elif ch in ['start simulation', 0]:
       self.program.start_data_generation()
       return self.main_menu()
     elif ch in ['stop simulation', 1]:
       self.program.stop_data_generation()
       return self.main_menu()
-    elif ch in ['vehicle functions', 2]:
+    elif ch in ['show vehicle functions', 2]:
       pass
-    elif ch in ['backend data', 3]:
-      pass
-
+    elif ch in ['show backend data stream', 3]:
+      if self.program.data_gen_on:
+        self.program.backend.show_data_stream()
+      else:
+        c_err("AVS-BAK-COMM", "DATA GENERATION IS OFF")
+        return self.main_menu()
     return self.main_menu()
 
   def start_frontend(self):
